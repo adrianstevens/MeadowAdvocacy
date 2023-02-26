cc9eab555ce583f28fb8966d51aef224a40cb55e
Adrian Stevens <adrian.stevens@gmail.com>
2023-02-25 22:05:13 -0800

    Add Cube sample

--- WaterQuality/MeadowApp.cs
+++ WaterQuality/MeadowApp.cs
@@ -1,7 +1,9 @@
 ﻿using Meadow;
 using Meadow.Devices;
 using Meadow.Foundation;
+using Meadow.Foundation.Sensors.Environmental;
 using Meadow.Hardware;
+using Meadow.Logging;
 using Meadow.Peripherals.Sensors.Location.Gnss;
 using System;
 using System.Threading.Tasks;
@@ -23,24 +25,34 @@ namespace AirQualityTracker
             return base.Run();
         }
 
-        public override Task Initialize()
+        public override async Task Initialize()
         {
             Console.WriteLine("Initialize...");
 
             projLab = ProjectLab.Create();
 
-            IDigitalOutputPort chipSelectPort, resetPort;
+            var client = projLab.GetModbusRtuClient(9600);
 
-            
-            if (projLab is ProjectLabHardwareV2 { } projlabV2)
+            var sensor = new Y4000(client, 1);
+            sensor.Initialize();
+
+            while (true)
             {
-            }
-            else
-            {
+                try
+                {
+                    var address = await sensor.GetISDN();
+                   // var serial = await sensor.GetSerialNumber();
+                    Console.WriteLine($"{address}");
+                }
+                catch (Exception ex) 
+                {
+                    Resolver.Log.Error($"Exception: {ex}");
+                }
+
+                await Task.Delay(5000);
             }
 
             Console.WriteLine("Init complete");
-            return base.Initialize();
         }
     }
 }
