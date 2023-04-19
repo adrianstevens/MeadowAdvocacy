f8af65e90c7d74b31b0bd5dabb811857f65e019a
Adrian Stevens <adrian.stevens@gmail.com>
2023-04-19 11:29:59 -0700

    Add Froggit

---
+++ Froggit/FrogItGame.Renderer.cs
@@ -0,0 +1,184 @@
+﻿using Meadow.Foundation.Graphics;
+using System;
+
+namespace Froggit
+{
+    public partial class FrogItGame
+    {
+        readonly byte cellSize = 16;
+
+        public void Init(MicroGraphics gl)
+        {
+            InitBuffers();
+
+            gl.Clear();
+            gl.DrawText(0, 0, "Meadow FrogIt");
+            gl.DrawText(0, 16, "v0.5.0");
+
+            gl.DrawBuffer(32, 32, frogUp);
+
+            gl.Show();
+        }
+
+        public void Update(MicroGraphics gl)
+        {
+            Update();
+
+            gl.Clear();
+            DrawBackground(gl);
+            DrawLanesAndCheckCollisions(gl);
+            DrawFrog(gl, frogState);
+            // DrawLives();
+            gl.Show();
+        }
+
+        void DrawBackground(MicroGraphics graphics)
+        {
+            //draw docks
+            for (int i = 0; i < 5; i++)
+            {
+                //  graphics.DrawRectangle(10 + 24 * i, 0, 12, 8, true, false);
+
+                if (i < FrogsHome)
+                {
+                    DrawFrog(12 + 24 * i, 0, FrogState.Forward, graphics);
+                }
+            }
+
+            //draw water
+            //graphics.DrawRectangle(0, cellSize, 128, cellSize * 3, true, true);
+        }
+
+        void DrawLanesAndCheckCollisions(MicroGraphics graphics)
+        {
+            int startPos, index, x, y;
+            int cellOffset;
+
+            double offsetD;
+
+            for (byte row = 0; row < 6; row++)
+            {
+                startPos = (int)(GameTime * LaneSpeeds[row]) % LaneLength;
+                offsetD = 16.0 * GameTime * LaneSpeeds[row];
+
+                cellOffset = ((int)offsetD) % cellSize;
+
+                if (startPos < 0)
+                {
+                    startPos = LaneLength - ((0 - startPos) % 32);
+                }
+
+                y = cellSize * (row + 1);
+
+                if (row < 3 && y == FrogY)
+                {
+                    FrogX -= TimeDelta * LaneSpeeds[row] * CellSize;
+                }
+
+                for (byte i = 0; i < Columns + 2; i++)
+                {
+                    index = LaneData[row, (startPos + i) % LaneLength];
+
+                    x = (i - 1) * cellSize - cellOffset;
+
+                    if (index == 0)
+                    {
+                        if (row < 3)
+                        {
+                            if (IsFrogCollision(x, y) == true)
+                            {
+                                KillFrog();
+                            }
+                        }
+                        continue;
+                    }
+
+                    if (x < 0 || x >= graphics.Width - CellSize)
+                        continue;
+
+                    switch (row)
+                    {
+                        case 0:
+                        case 1:
+                        case 2:
+                            DrawLog(x, y, index, graphics);
+                            break;
+                        case 3:
+                        case 5:
+                            DrawTruck(x, y, index, graphics);
+                            if (IsFrogCollision(x, y)) { KillFrog(); }
+                            break;
+                        case 4:
+                            DrawCar(x, y, index, graphics);
+                            if (IsFrogCollision(x, y)) { KillFrog(); }
+                            break;
+                    }
+                }
+            }
+        }
+
+        bool IsFrogCollision(int x, int y)
+        {
+            if (y == FrogY &&
+                x > FrogX &&
+                x < FrogX + cellSize)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        void DrawLives(MicroGraphics graphics)
+        {
+            for (int i = 1; i < Lives; i++)
+            {
+                DrawFrog(cellSize * (Columns - i), cellSize * (Rows - 1), FrogState.Forward, graphics);
+            }
+        }
+
+        void DrawFrog(MicroGraphics graphics, FrogState state = FrogState.Forward)
+        {
+            DrawFrog((int)Math.Truncate(FrogX), (int)Math.Truncate(FrogY), state, graphics);
+        }
+
+        void DrawFrog(int x, int y, FrogState state, MicroGraphics graphics)
+        {
+            if (state == FrogState.Left)
+            {
+                graphics.DrawBuffer(x, y, frogLeft);
+            }
+            else if (state == FrogState.Forward)
+            {
+                graphics.DrawBuffer(x, y, frogUp);
+            }
+            else if (state == FrogState.Right)
+            {
+                graphics.DrawBuffer(x, y, frogRight);
+            }
+            else
+            {
+                graphics.DrawText(x, y, "X");
+            }
+        }
+
+        void DrawTruck(int x, int y, int index, MicroGraphics graphics)
+        {
+            if (index == 1) graphics.DrawBuffer(x, y, truckLeft);
+            else if (index == 2) graphics.DrawBuffer(x, y, truckCenter);
+            else if (index == 3) graphics.DrawBuffer(x, y, truckRight);
+        }
+
+        void DrawLog(int x, int y, int index, MicroGraphics graphics)
+        {
+            if (index == 1) graphics.DrawBuffer(x, y, logDarkLeft);
+            else if (index == 2) graphics.DrawBuffer(x, y, logDarkCenter);
+            else if (index == 3) graphics.DrawBuffer(x, y, logDarkRight);
+        }
+
+        void DrawCar(int x, int y, int index, MicroGraphics graphics)
+        {
+            if (index == 1) graphics.DrawBuffer(x, y, carLeft);
+            else if (index == 2) graphics.DrawBuffer(x, y, carRight);
+        }
+    }
+}
---
+++ Froggit/MeadowApp.cs
@@ -0,0 +1,78 @@
+﻿using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation.Graphics;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using WildernessLabs.Hardware.Juego;
+
+namespace Froggit
+{
+    // Change F7FeatherV2 to F7FeatherV1 for V1.x boards
+    public class MeadowApp : App<F7CoreComputeV2>
+    {
+        IJuegoHardware juego;
+        FrogItGame game;
+        MicroGraphics graphics;
+
+        bool playGame = true;
+
+        public override Task Initialize()
+        {
+            Console.WriteLine("Initialize...");
+
+            juego = Juego.Create();
+            juego.Left_DownButton.Clicked += Left_DownButton_Clicked;
+            juego.Left_UpButton.Clicked += Left_UpButton_Clicked;
+            juego.Left_LeftButton.Clicked += Left_LeftButton_Clicked;
+            juego.Left_RightButton.Clicked += Left_RightButton_Clicked;
+
+            graphics = new MicroGraphics(juego.Display);
+            graphics.CurrentFont = new Font8x12();
+            graphics.Rotation = RotationType._270Degrees;
+
+            game = new FrogItGame();
+            game.Init(graphics);
+
+
+            return base.Initialize();
+        }
+
+        private void Left_RightButton_Clicked(object sender, EventArgs e)
+        {
+            game.Right();
+        }
+
+        private void Left_LeftButton_Clicked(object sender, EventArgs e)
+        {
+            game.Left();
+        }
+
+        private void Left_UpButton_Clicked(object sender, EventArgs e)
+        {
+            game.Up();
+        }
+
+        private void Left_DownButton_Clicked(object sender, EventArgs e)
+        {
+            game.Down();
+        }
+
+        public async override Task Run()
+        {
+            Console.WriteLine("Run...");
+
+            await Task.Run(() =>
+            {   //full speed today
+                while (playGame == true)
+                {
+                    game.Update(graphics);
+
+                    Thread.Sleep(0);
+                }
+            });
+        }
+
+
+    }
+}
