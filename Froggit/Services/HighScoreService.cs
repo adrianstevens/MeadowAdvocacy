4be764d73b43f9f0e955b014e368fe36cff0e585
Adrian Stevens <adrian.stevens@gmail.com>
2023-04-27 12:21:38 -0700

    Wrap PostTime in try/catch

--- Froggit/Services/HighScoreService.cs
+++ Froggit/Services/HighScoreService.cs
@@ -14,39 +14,46 @@ namespace Froggit.Services
 
         public static async Task PostTime(string name, int time, int deaths)
         {
-            Console.WriteLine("PostTime ... ");
-            var score = new FastestTime()
+            try
             {
-                PlayerName = name,
-                Time = time,
-                Deaths = deaths
-            };
+                Console.WriteLine("PostTime ... ");
+                var score = new FastestTime()
+                {
+                    PlayerName = name,
+                    Time = time,
+                    Deaths = deaths
+                };
 
-            var serializer = new JsonSerializer();
+                var serializer = new JsonSerializer();
 
-            Console.WriteLine($"Serialize {score}");
-            string json = serializer.Serialize(score);
+                Console.WriteLine($"Serialize {score}");
+                string json = serializer.Serialize(score);
 
-            Console.WriteLine("Create an HttpClient instance");
-            using HttpClient httpClient = new HttpClient();
+                Console.WriteLine("Create an HttpClient instance");
+                using HttpClient httpClient = new HttpClient();
 
-            Console.WriteLine("Set the base address for the API");
-            httpClient.BaseAddress = new Uri(ScoreUrl);
+                Console.WriteLine("Set the base address for the API");
+                httpClient.BaseAddress = new Uri(ScoreUrl);
 
-            Console.WriteLine("Create the HttpContent for the JSON payload");
-            HttpContent content = new StringContent(json, Encoding.UTF8, "application/json");
+                Console.WriteLine("Create the HttpContent for the JSON payload");
+                HttpContent content = new StringContent(json, Encoding.UTF8, "application/json");
 
-            Console.WriteLine("Send the POST request to the API");
-            HttpResponseMessage response = await httpClient.PostAsync(ApiEndpoint, content);
+                Console.WriteLine("Send the POST request to the API");
+                HttpResponseMessage response = await httpClient.PostAsync(ApiEndpoint, content);
 
-            Console.WriteLine("Check the response status");
-            if (response.IsSuccessStatusCode)
-            {
-                Console.WriteLine("Data sent successfully!");
+                Console.WriteLine("Check the response status");
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("Data sent successfully!");
+                }
+                else
+                {
+                    Console.WriteLine("Error sending data: " + response.StatusCode);
+                }
             }
-            else
+            catch (Exception ex)
             {
-                Console.WriteLine("Error sending data: " + response.StatusCode);
+                Console.WriteLine(ex.Message);
             }
         }
     }
