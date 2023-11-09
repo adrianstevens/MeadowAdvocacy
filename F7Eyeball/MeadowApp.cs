9486aa23965b5f45d1cf037c81c06a79d25b39d4
Adrian Stevens <adrian.stevens@gmail.com>
2023-11-08 21:03:07 -0800

    Add F7 eyeball project

---
+++ F7Eyeball/MeadowApp.cs
@@ -0,0 +1,51 @@
+﻿using HalloweenEyeball;
+using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation.Displays;
+using Meadow.Foundation.Leds;
+using Meadow.Peripherals.Leds;
+using System;
+using System.Threading.Tasks;
+
+namespace F7Eyeball
+{
+    public class MeadowApp : App<F7FeatherV1>
+    {
+        EyeballController eyeballController;
+
+        public override Task Initialize()
+        {
+            Console.WriteLine("Initialize...");
+
+            var spibus = Device.CreateSpiBus(Device.Pins.SCK, Device.Pins.MOSI, Device.Pins.MISO, new Meadow.Units.Frequency(48000, Meadow.Units.Frequency.UnitType.Kilohertz));
+            var display = new Ili9341(spibus, Device.Pins.D13, Device.Pins.D14, Device.Pins.D15, 240, 320, Meadow.Foundation.Graphics.ColorMode.Format12bppRgb444);
+
+            eyeballController = new EyeballController(display);
+
+            var onboardLed = new RgbPwmLed(
+                redPwmPin: Device.Pins.OnboardLedRed,
+                greenPwmPin: Device.Pins.OnboardLedGreen,
+                bluePwmPin: Device.Pins.OnboardLedBlue,
+                CommonType.CommonAnode);
+
+            onboardLed.StartPulse(Meadow.Foundation.Color.Red);
+
+            return Task.CompletedTask;
+        }
+
+        public override Task Run()
+        {
+            Console.WriteLine("Run...");
+
+            eyeballController.DrawEyeball();
+
+            while (true)
+            {
+                eyeballController.Delay();
+                eyeballController.RandomEyeMovement();
+                eyeballController.Delay();
+                eyeballController.CenterEye();
+            }
+        }
+    }
+}
--- WaterQuality/MeadowApp.cs
+++ WaterQuality/MeadowApp.cs
@@ -1,69 +1,34 @@
 ﻿using Meadow;
 using Meadow.Devices;
-using Meadow.Foundation;
-using Meadow.Foundation.Sensors.Environmental;
 using System;
 using System.Threading.Tasks;
 
-namespace AirQualityTracker
+namespace WaterQualityTracker
 {
     // Change F7FeatherV2 to F7FeatherV1 for V1.x boards
     public class MeadowApp : App<F7CoreComputeV2>
     {
+        WaterQualityController waterQualityController;
+
         IProjectLabHardware projLab;
 
         public override Task Run()
         {
             Console.WriteLine("Run...");
 
-            projLab.Display.Fill(Color.Red);
-            projLab.Display.Show();
+            waterQualityController.Run();
 
-            return base.Run();
+            return Task.CompletedTask;
         }
 
-        public override async Task Initialize()
+        public async override Task Initialize()
         {
             Console.WriteLine("Initialize...");
 
             projLab = ProjectLab.Create();
 
-            var client = projLab.GetModbusRtuClient(9600);
-
-            var sensor = new Y4000(client, 1);
-
-            await sensor.Initialize();
-
-            for (int i = 0; i < 3; i++)
-            {
-                try
-                {
-                    var address = await sensor.GetISDN();
-                    Console.WriteLine($"Address: {address}");
-
-                    var serial = await sensor.GetSerialNumber();
-                    Console.WriteLine($"Serial: {serial}");
-                }
-                catch (Exception ex)
-                {
-                    Resolver.Log.Error($"Exception: {ex}");
-                }
-
-                await Task.Delay(5000);
-            }
-
-            sensor.Updated += Sensor_Updated;
-
-            sensor.StartUpdating();
-
-            Console.WriteLine("Init complete");
-        }
-
-        private void Sensor_Updated(object sender, IChangeResult<(Meadow.Units.ConcentrationInWater? DisolvedOxygen, Meadow.Units.ConcentrationInWater? Chlorophyl, Meadow.Units.ConcentrationInWater? BlueGreenAlgae, Meadow.Units.Conductivity? ElectricalConductivity, Meadow.Units.PotentialHydrogen? PH, Meadow.Units.Turbidity? Turbidity, Meadow.Units.Temperature? Temperature, Meadow.Units.Voltage? OxidationReductionPotential)> e)
-        {
-            Console.WriteLine($"Temp: {e.New.Temperature.Value.Celsius}C");
-            Console.WriteLine($"pH: {e.New.PH.Value}pH");
-            Console.WriteLine($"Disolved O2: {e.New.DisolvedOxygen.Value}");
+            waterQualityController = new WaterQualityController();
+            await waterQualityController.Initialize(projLab);
         }
     }
 }
