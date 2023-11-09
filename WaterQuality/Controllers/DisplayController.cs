84345c4f9f7cf133c8e26e9dec2dd18945f9c542
Adrian Stevens <adrian.stevens@gmail.com>
2023-11-08 21:03:21 -0800

    Update WaterQuality project

---
+++ WaterQuality/Controllers/DisplayController.cs
@@ -0,0 +1,38 @@
+﻿using Meadow.Devices;
+using Meadow.Foundation;
+using Meadow.Foundation.Graphics;
+
+namespace WaterQualityTracker
+{
+    internal class DisplayController
+    {
+        readonly MicroGraphics graphics;
+
+        readonly IProjectLabHardware projectLab;
+
+        public DisplayController(IGraphicsDisplay display)
+        {
+            graphics = new MicroGraphics(display);
+        }
+
+        public void UpdateDisplay(
+           (Meadow.Units.ConcentrationInWater? DissolvedOxygen,
+            Meadow.Units.ConcentrationInWater? Chlorophyl,
+            Meadow.Units.ConcentrationInWater? BlueGreenAlgae,
+            Meadow.Units.Conductivity? ElectricalConductivity,
+            Meadow.Units.PotentialHydrogen? PH, Meadow.Units.Turbidity? Turbidity,
+            Meadow.Units.Temperature? Temperature, Meadow.Units.Voltage? OxidationReductionPotential) data)
+        {
+            graphics.Clear();
+
+            graphics.DrawText(0, 0, "Y4000 Water Quality", Color.Red, ScaleFactor.X2);
+
+            graphics.DrawText(0, 60, $"Temp: {data.Temperature.Value.Celsius:0.0}C", Color.White, ScaleFactor.X2);
+            graphics.DrawText(0, 90, $"pH: {data.PH.Value:0.0}pH", Color.White, ScaleFactor.X2);
+            graphics.DrawText(0, 120, $"Disolved O2: {data.DissolvedOxygen.Value:0.0}", Color.White, ScaleFactor.X2);
+            graphics.DrawText(0, 150, $"Algae: {data.BlueGreenAlgae.Value:0.0}", Color.White, ScaleFactor.X2);
+
+            graphics.Show();
+        }
+    }
+}
---
+++ WaterQuality/Controllers/WaterQualityController.cs
@@ -0,0 +1,49 @@
+﻿using Meadow.Devices;
+using Meadow.Foundation.Sensors.Environmental;
+using System;
+using System.Threading.Tasks;
+
+namespace WaterQualityTracker
+{
+    internal class WaterQualityController
+    {
+        DisplayController displayController;
+
+        Y4000 sensor;
+
+        public async Task Initialize(IProjectLabHardware projectLab)
+        {
+            displayController = new DisplayController(projectLab.Display);
+
+            var client = projectLab.GetModbusRtuClient(9600);
+
+            sensor = new Y4000(client, 1);
+
+            sensor.Updated += Y4000Updated;
+
+            await sensor.Initialize();
+        }
+
+        public void Run()
+        {
+            sensor.StartUpdating(TimeSpan.FromSeconds(1));
+        }
+
+        private void Y4000Updated(object sender,
+            Meadow.IChangeResult<(Meadow.Units.ConcentrationInWater? DissolvedOxygen,
+                Meadow.Units.ConcentrationInWater? Chlorophyl,
+                Meadow.Units.ConcentrationInWater? BlueGreenAlgae,
+                Meadow.Units.Conductivity? ElectricalConductivity,
+                Meadow.Units.PotentialHydrogen? PH,
+                Meadow.Units.Turbidity? Turbidity,
+                Meadow.Units.Temperature? Temperature,
+                Meadow.Units.Voltage? OxidationReductionPotential)> e)
+        {
+            displayController.UpdateDisplay(e.New);
+
+            Console.WriteLine($"Temp: {e.New.Temperature.Value.Celsius:0.0}C");
+            Console.WriteLine($"pH: {e.New.PH.Value:0.0}pH");
+            Console.WriteLine($"Disolved O2: {e.New.DissolvedOxygen.Value:0.0}");
+        }
+    }
+}
