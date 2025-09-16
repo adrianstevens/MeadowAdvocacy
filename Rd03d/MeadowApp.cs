d5923ad0818a8580678f84a96d09064cae6a701e
Adrian Stevens <adrian.stevens@gmail.com>
2025-09-16 13:35:12 -0700

    Start of RD-03D driver

---
+++ Rd03d/MeadowApp.cs
@@ -0,0 +1,31 @@
+﻿using Meadow;
+using Meadow.Devices;
+using System;
+using System.Threading.Tasks;
+
+namespace ArducamMini
+{
+    public class MeadowApp : App<F7CoreComputeV2>
+    {
+        IProjectLabHardware projLab;
+
+        public override Task Initialize()
+        {
+            Console.WriteLine("Initialize...");
+
+            projLab = ProjectLab.Create();
+
+
+            Console.WriteLine("initialized");
+
+
+            return Task.CompletedTask;
+        }
+
+        public override async Task Run()
+        {
+            Console.WriteLine("Run...");
+
+        }
+    }
+}
---
+++ Rd03d/driver/Rd03d.cs
@@ -0,0 +1,48 @@
+﻿using Meadow.Hardware;
+using System;
+
+namespace Meadow.Foundation.Sensors.Motion;
+
+/// <summary>
+/// Represents and RD-03D motion sensor
+/// </summary>
+public abstract partial class Rd03d
+{
+    readonly ISerialMessagePort? serialMessagePort;
+
+    static readonly byte[] suffixDelimiter = { 13 }; //place holder, this might be wrong
+    static readonly int portSpeed = 256000;
+
+    /// <summary>
+    /// Did we create the port(s) used by the peripheral
+    /// </summary>
+    readonly bool createdPorts = false;
+
+    DateTime lastUpdate = DateTime.MinValue;
+
+    /// <summary>
+    /// Creates a new Rd03d object communicating over serial
+    /// </summary>
+    /// <param name="device">The device connected to the sensor</param>
+    /// <param name="serialPortName">The serial port name</param>
+    public Rd03d(IMeadowDevice device, SerialPortName serialPortName) :
+        this(device.CreateSerialMessagePort(serialPortName, suffixDelimiter, false, baudRate: portSpeed))
+    {
+        createdPorts = true;
+    }
+
+    /// <summary>
+    /// Creates a new Rd03d object communicating over serial
+    /// </summary>
+    /// <param name="serialMessage">The serial message port</param>
+    public Rd03d(ISerialMessagePort serialMessage)
+    {
+        serialMessagePort = serialMessage;
+        serialMessagePort.MessageReceived += SerialMessagePort_MessageReceived;
+    }
+
+    private void SerialMessagePort_MessageReceived(object sender, SerialMessageData e)
+    {
+        Console.WriteLine($"Received: {e.Message}");
+    }
+}
